3728379e689a5a0265076921e05c06eb38bfbe55
Khổng Tám <22690521.tam@student.iuh.edu.vn>
2026-02-25 10:49:38 +0700

    feat: add Api to hide conversation

--- apps/backend/src/modules/conversation-settings/conversation-settings.controller.ts
+++ apps/backend/src/modules/conversation-settings/conversation-settings.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
 
+import { Body, Controller, Patch } from '@nestjs/common';
+import { ConversationSettingsService } from './conversation-settings.service';
+import { Types } from 'mongoose';
 @Controller('conversation-settings')
-export class ConversationSettingsController {}
+export class ConversationSettingsController {
+    constructor(
+        private readonly conversationSettingsService: ConversationSettingsService,
+    ) { }
+
+    // Ẩn
+    @Patch('hide')
+    hideConversation(
+        @Body('userId') userId: string,
+        @Body('conversationId') conversationId: string,
+    ) {
+        return this.conversationSettingsService.hideConversation(new Types.ObjectId(userId),
+            new Types.ObjectId(conversationId));
+
+    }
+}
--- apps/backend/src/modules/conversation-settings/conversation-settings.service.ts
+++ apps/backend/src/modules/conversation-settings/conversation-settings.service.ts
@@ -1,4 +1,37 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import {
+    ConversationSetting,
+    ConversationSettingDocument
+} from './schemas/conversation-setting.schema';
+import { Model, Types } from 'mongoose';
 @Injectable()
-export class ConversationSettingsService {}
+export class ConversationSettingsService {
+    constructor(
+        @InjectModel(ConversationSetting.name)
+        private readonly conversationSettingModel: Model<ConversationSettingDocument>,
+    ) { }
+
+    // Ẩn cuộc hội thoại
+    async hideConversation(
+        userId: Types.ObjectId,
+        conversationId: Types.ObjectId) {
+        const setting = await this.conversationSettingModel.findOneAndUpdate(
+            {
+                userId,
+                conversationId,
+            },
+            {
+                $set: { hidden: true },
+            },
+            {
+                new: true,
+                upsert: true,
+                setDefaultsOnInsert: true,
+            },
+        );
+        return setting;
+    }
+
+}
